eab470500269ede4cabbc36dbcd89d64d8785663
SAPARK <seulae46@naver.com>
2022-07-18 17:09:03 +0900

    state2

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목,b] = useState(['남자 코트 추천','여자 코트 추천']);
+  let [글제목,b] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={ {color:'white', fontSize : '16px'} }>블로그임</h4>
+        <h4 style={ {color:'white', fontSize : '16px'} }>ReactBlog</h4>
       </div>
       <div className='list'>
         <h4>{ 글제목[0] }</h4>
@@ -18,6 +18,10 @@ function App() {
       </div>
       <div className='list'>
         <h4>{ 글제목[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
